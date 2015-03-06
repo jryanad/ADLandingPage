da4cf61dc7c3128a35f038ad819a106a91ce6dcf
Jason Ryan <jason.ryan@audiologydesign.com>
2015-03-06 12:52:40 -0500

    Updates to core

--- form-handler.php
+++ form-handler.php
@@ -1,6 +1,6 @@
 <?php
 
-define("WEBMASTER_EMAIL", 'youremail@email.com');
+define("WEBMASTER_EMAIL", 'websites@audiologydesign.com');
 define("REPLY_TO", "$email");
 
 error_reporting (E_ALL ^ E_NOTICE);
--- js/vendor/custom.gmap.js
+++ js/vendor/custom.gmap.js
@@ -1,9 +1,11 @@
 function initialize() {
+	// Disable dragging on smaller mobile devices
   var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
   var isDraggable = w > 480 ? true : false;
   
+	// Set Center of the Map
   var mapOptions = {
-    center: new google.maps.LatLng(40.247162,-75.243832),
+    center: new google.maps.LatLng(29.8047377,-95.4077045),
     disableDefaultUI: true,
     styles: flatMap,
     zoomControl: true,
@@ -17,7 +19,8 @@ function initialize() {
     disableDoubleClickZoom: true,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-
+	
+	// Define Map Conatainer ID
   var map = new google.maps.Map(document.getElementById("map-wrapper"),
       mapOptions);
 
@@ -25,52 +28,40 @@ function initialize() {
   var bounds = new google.maps.LatLngBounds();
   var marker, i;
 
-  // Bethlehem Location
+  // Location Pin
+	// Copy this block to add more markers on the map
+	
   var myIcon = new google.maps.MarkerImage("http://cdn.audiologydesign.com/ppc/images/map-marker.png", null, null, null, new google.maps.Size(30,30));
                                      
   marker = new google.maps.Marker({
-    position: new google.maps.LatLng(40.261905,-75.248958),
+    position: new google.maps.LatLng(29.8047377,-95.4077045),
     map: map,
     icon: myIcon
   });
 
   bounds.extend(marker.position);
+	
+	// Add Text for Map Info Window
   google.maps.event.addListener(marker, "mousedown", (function(marker, i) {
     return function() {
-      infowindow.setContent("<div class='map-info-box'><p><strong>Listen 2 Life Hearing Center</strong><br />595 Bethlehem Pike<br />Suite 301<br />Montgomeryville, PA 18936</p></div>");
+      infowindow.setContent("<div class='map-info-box'><p><strong>AudiologyDesign</strong><br />538 West 21st St.<br />Suite #79931<br />Houston, TX 77008-3642</p></div>");
     infowindow.open(map, marker);
       }
   })(marker, i));
 
   map.fitBounds(bounds);
+	
+	// End Location Block
 
-  // Phoenixville Location
-  var myIcon = new google.maps.MarkerImage("http://cdn.audiologydesign.com/ppc/images/map-marker.png", null, null, null, new google.maps.Size(30,30));
-                                     
-  marker = new google.maps.Marker({
-    position: new google.maps.LatLng(40.131428,-75.450874),
-    map: map,
-    icon: myIcon
-  });
-
-  bounds.extend(marker.position);
-  google.maps.event.addListener(marker, "mousedown", (function(marker, i) {
-    return function() {
-      infowindow.setContent("<div class='map-info-box'><p><strong>Listen 2 Life Hearing Center</strong><br />595 Bethlehem Pike<br />Suite 301<br />Montgomeryville, PA 18936</p></div>");
-    infowindow.open(map, marker);
-      }
-  })(marker, i));
-
-  map.fitBounds(bounds);
-
-  //Set Zoom Level
+  //Set Zoom Level 1-21 Default is 10
   google.maps.event.addListenerOnce(map, 'bounds_changed', function() {
     map.setZoom(10);
   });
 
-  //Responsive Handler
+  // Responsive Handler
+	// Reset Map Center on Window Resize
   google.maps.event.addDomListener(window, "resize", function() {
-    var center = new google.maps.LatLng(40.247162,-75.243832);
+    var center = new google.maps.LatLng(29.8047377,-95.4077045);
     google.maps.event.trigger(map, "resize");
     map.setCenter(center);
   });
